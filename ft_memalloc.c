f0e354bf39d35a9e950357bb66e9b651089740db
Jeremy SCHOTTE <jschotte@e3r1p2.42.fr>
2015-12-02 11:33:03 +0100

    basic libft

---
+++ ft_memalloc.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschotte <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/26 16:18:42 by jschotte          #+#    #+#             */
+/*   Updated: 2015/11/30 15:53:01 by jschotte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_memalloc(size_t size)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = malloc(size);
+	if (str == NULL)
+		return (NULL);
+	else
+	{
+		while (i < size)
+		{
+			str[i] = 0;
+			i++;
+		}
+		return (str);
+	}
+}
